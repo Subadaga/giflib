d6535eef70460a42682bf0e2435b62638789d9d7
Sergio Sánchez Basoco <A01339507@itesm.mx>
2017-07-10 01:09:29 -0500

    Initial commit

---
+++ src/main/java/com/irina/giflib/AppConfig.java
@@ -0,0 +1,16 @@
+package com.irina.giflib;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+
+/**
+ * Created by Amiga on 03/07/2017.
+ */
+@EnableAutoConfiguration
+public class AppConfig {
+
+    public static void main(String[] args){
+        SpringApplication.run(AppConfig.class, args);
+    }
+
+}
